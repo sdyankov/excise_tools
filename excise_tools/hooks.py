d08ec6f78be50539607e33d65c983dbf0781556d
sdyankov <sdyankov@yahoo.com>
2025-08-24 20:15:46 +0300

    Delete excise_tools directory

--- excise_tools/hooks.py
+++
@@ -1,6 +0,0 @@
-app_name = "excise_tools"
-app_title = "Excise Tools"
-app_publisher = "Premitium"
-app_description = "Excise duty and customs tools for ERPNext"
-app_email = "info@example.com"
-app_license = "MIT"
