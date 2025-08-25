a84094357363d09c66598067555c65eefe3b3f22
sdyankov <sdyankov@yahoo.com>
2025-08-25 03:33:12 +0300

    Update hooks.py

--- excise_tools/hooks.py
+++ excise_tools/hooks.py
@@ -4,3 +4,7 @@ app_publisher = "Premitium"
 app_description = "Excise duty and customs tools for ERPNext"
 app_email = "info@example.com"
 app_license = "MIT"
+
+
+app_include_css = "/assets/excise_tools/css/excise_tools.css"
+app_include_js = "/assets/excise_tools/js/excise_tools.bundle.js"
