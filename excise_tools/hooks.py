a896494b0a6d7a3b414a5f500a67601eb252fd8d
sdyankov <sdyankov@yahoo.com>
2025-08-25 03:48:25 +0300

    Update hooks.py

--- excise_tools/hooks.py
+++ excise_tools/hooks.py
@@ -5,6 +5,5 @@ app_description = "Excise duty and customs tools for ERPNext"
 app_email = "info@example.com"
 app_license = "MIT"
 
-
-app_include_css = "/assets/excise_tools/css/excise_tools.css"
-app_include_js = "/assets/excise_tools/js/excise_tools.bundle.js"
+app_include_css = "assets/excise_tools/css/excise_tools.min.css"
+app_include_js  = "assets/excise_tools/js/excise_tools.min.js"
